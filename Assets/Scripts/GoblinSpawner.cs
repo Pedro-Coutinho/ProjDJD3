c35e563e7e368e32a592da8e8f8cf931616a5106
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2021-03-11 18:09:15 +0000

    Add post processing

---
+++ Assets/Scripts/GoblinSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSpawner : MonoBehaviour
+{
+    public GameObject goblinSpawner;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            goblinSpawner.SetActive(true);
+        }
+    }
+}
